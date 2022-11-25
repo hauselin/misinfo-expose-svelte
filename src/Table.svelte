d32069ab09c390236dd9a22ec9bef354b2e7399d
Puneet Bhargava <pbhargava936@gmail.com>
2022-11-25 14:34:14 -0800

    add table

--- src/Table.svelte
+++ src/Table.svelte
@@ -1,4 +1,9 @@
 <script>
+    import { data } from './data.example.js'  
+    import { Datatable } from 'svelte-simple-datatables'
+
+    const settings = { columnFilter: true }
+    let rows
 </script>
 
 <main>
@@ -6,7 +11,29 @@
 		The estimates above are based on these 10 users <strong>dg_rand</strong>
 		follows. Falsity scores for the elites are also shown.
 	</p>
+
+
 </main>
 
+<Datatable settings={settings} data={data} bind:dataRows={rows}>
+    <thead>
+        <th data-key="first_name">First Name</th>
+        <th data-key="last_name">Last Name</th>
+        <th data-key="email">Email</th>
+    </thead>
+    <tbody>
+    {#if rows}
+        {#each $rows as row}
+        <tr>
+            <td>{row.first_name}</td>
+            <td>{row.last_name}</td>
+            <td>{row.email}</td>
+        </tr>
+        {/each}
+    {/if}
+    </tbody>
+</Datatable>
+
 <style>
+	td{text-align:center;padding:4px 0; color:#000000; background-color:#979BCA;}
 </style>
---
+++ src/data.example.js
@@ -0,0 +1,22 @@
+export const data = [
+  { "id": 1, "first_name": "Tobie", "last_name": "Vint", "email": "tvint0@fotki.com" },
+  { "id": 2, "first_name": "Zacharias", "last_name": "Cerman", "email": "zcerman1@sciencedirect.com" },
+  { "id": 3, "first_name": "Gerianna", "last_name": "Bunn", "email": "gbunn2@foxnews.com" },
+  { "id": 4, "first_name": "Bee", "last_name": "Saurin", "email": "bsaurin3@live.com" },
+  { "id": 5, "first_name": "Meyer", "last_name": "Garnul", "email": "mgarnul4@yellowbook.com" },
+  { "id": 6, "first_name": "Frederich", "last_name": "Benley", "email": "fbenley5@ameblo.jp" },
+  { "id": 7, "first_name": "Becki", "last_name": "Criag", "email": "bcriag6@washingtonpost.com" },
+  { "id": 8, "first_name": "Nichols", "last_name": "Risom", "email": "nrisom7@google.com.br" },
+  { "id": 9, "first_name": "Ron", "last_name": "Menendes", "email": "rmenendes8@prnewswire.com" },
+  { "id": 10, "first_name": "Thane", "last_name": "Gammill", "email": "tgammill9@com.com" },
+  { "id": 11, "first_name": "Ramonda", "last_name": "Yakobowitch", "email": "ryakobowitcha@hibu.com" },
+  { "id": 12, "first_name": "Rosalynd", "last_name": "Connechie", "email": "rconnechieb@chicagotribune.com" },
+  { "id": 13, "first_name": "Vinny", "last_name": "Hissett", "email": "vhissettc@4shared.com" },
+  { "id": 14, "first_name": "Mylo", "last_name": "Tarbath", "email": "mtarbathd@behance.net" },
+  { "id": 15, "first_name": "Pierson", "last_name": "Linny", "email": "plinnye@altervista.org" },
+  { "id": 16, "first_name": "Adi", "last_name": "Bassano", "email": "abassanof@about.com" },
+  { "id": 17, "first_name": "Paxon", "last_name": "Claypole", "email": "pclaypoleg@storify.com" },
+  { "id": 18, "first_name": "Marcella", "last_name": "Mogey", "email": "mmogeyh@weebly.com" },
+  { "id": 19, "first_name": "Fiann", "last_name": "Winsborrow", "email": "fwinsborrowi@icq.com" },
+  { "id": 20, "first_name": "Moria", "last_name": "Druhan", "email": "mdruhanj@friendfeed.com" }
+]
