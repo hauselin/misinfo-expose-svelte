139d958230afb8c54aba5b145b59becac248b250
hauselin <hauselin@gmail.com>
2022-11-26 14:18:48 -0500

    Tooltip

--- src/vega-specs/exposure_density.js
+++ src/vega-specs/exposure_density.js
@@ -12,14 +12,18 @@ export default {
 		"transform": [{ "density": "value", "bandwidth": 0.0, "extent": [0, 1] }],
 		"encoding": {
 			"x": { "field": "value", "title": "", "type": "quantitative", "axis": { "domain": false, "grid": false, "ticks": false, "labelColor": "white", } },
-			"y": { "field": "density", "type": "quantitative", "title": "", "axis": { "domain": false, "grid": false, "ticks": false, "labels": false } }
+			"y": {
+				"field": "density", "type": "quantitative", "title": "", "axis": { "domain": false, "grid": false, "ticks": false, "labels": false }
+			},
+			"tooltip": { "field": "value" }
 		},
 	}, {
 		"mark": { "type": "rule", "color": "#91e5f6" },
 		"data": { "values": [{ "value": 0.5 }] },
 		"encoding": {
 			"x": { "field": "value", "title": "", "type": "quantitative", "axis": { "domain": false, "grid": false, "ticks": false, "labelColor": "white", } },
-			"size": { "value": 4 }
+			"size": { "value": 4 },
+			"tooltip": { "field": "value" }
 		}
 	}],
 	"width": 300,
